87c6eeed57b634fa4b9bea95fed26d41e8f507a4
NBojan <63514753+NBojan@users.noreply.github.com>
2022-11-08 06:49:23 -0600

    before start

---
+++ functions/nb-all.js
@@ -0,0 +1,33 @@
+const Airtable = require('airtable-node');
+require("dotenv").config();
+ 
+const airtable = new Airtable({ apiKey: process.env.REACT_APP_AIR_KEY })
+  .base('appS9I5vk3LgeuBdc')
+  .table('projects');
+
+exports.handler = async (event, context) => {
+    const data = await airtable.list({
+        maxRecords: 50,
+        pageSize: 50
+    });
+
+    if(data.records){
+        const projects = data.records.map(project => {
+            const { id, fields: {name, descr, images, url, gitUrl} } = project;
+            return { id, name, descr, images, url, gitUrl }
+        })
+        return {
+            headers: {
+                "Access-Control-Allow-Origin": "*"
+            },
+            statusCode: 200,
+            body: JSON.stringify(projects)
+        }
+    }
+    else {
+        return {
+            statusCode: 500,
+            body: "There was an error."
+        }
+    }
+}
---
+++ functions/nb-single.js
@@ -0,0 +1,37 @@
+const Airtable = require('airtable-node');
+require("dotenv").config();
+ 
+const airtable = new Airtable({ apiKey: process.env.REACT_APP_AIR_KEY })
+  .base('appS9I5vk3LgeuBdc')
+  .table('projects');
+
+  exports.handler = async (event, context) => {
+    const { id } = event.queryStringParameters;
+
+    if(id){
+        const project = await airtable.retrieve(id);
+        if(project.error){
+            return {
+                statusCode: 404,
+                body: JSON.stringify(`No item with this '${id}' id.`)
+            }
+        }
+        else {
+            const { id, fields: { url, gitUrl, images, name, descr } } = project;
+            const singleProject = { id, url, gitUrl, images, name, descr };
+            return {
+                headers: {
+                    "Access-Control-Allow-Origin": "*"
+                },
+                statusCode: 200,
+                body: JSON.stringify(singleProject)
+            }
+        }
+    }
+    else {
+        return {
+            statusCode: 400,
+            body: JSON.stringify("Please provide an ID.")
+        }
+    }
+}
