70e0e32df0862b639d13198ab09cfcf0c20c85a9
NBojan <63514753+NBojan@users.noreply.github.com>
2023-02-25 09:01:11 -0600

    s

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,6 +1,6 @@
-import { Link } from "react-router-dom";
 import styled from "styled-components";
 import errorImg from "../assets/images/stars.svg";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
     return (  
